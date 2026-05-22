93c2693fc7d58d7667575753eb03366d4833f6b3
Dada Vincent <dadaademolav@gmail.com>
2026-05-22 01:09:45 +0100

    fix: add path alias and baseUrl

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import { nitro } from 'nitro/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
@@ -26,4 +27,9 @@ export default defineConfig({
     nitro(),
     viteReact(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
